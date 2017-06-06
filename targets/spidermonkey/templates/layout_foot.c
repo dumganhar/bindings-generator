16b2b89c826be939ef5ecc8b1234918fbe28b8d1
James Chen <jianhua.chen@cocos2d-x.org>
2017-06-06 22:02:08 +0800

    create obj

--- targets/spidermonkey/templates/layout_foot.c
+++ targets/spidermonkey/templates/layout_foot.c
@@ -3,7 +3,13 @@ bool register_all_${prefix}(se::Object* obj)
 #if $target_ns
     // Get the ns
     se::Value nsVal;
-    obj->getProperty("${target_ns}", &nsVal);
+    if (!obj->getProperty("${target_ns}", &nsVal))
+    {
+        se::Object* jsobj = se::Object::createPlainObject(false);
+        nsVal.setObject(jsobj);
+        obj->setProperty("${target_ns}", nsVal);
+        jsobj->release();
+    }
     se::Object* ns = nsVal.toObject();
 #else 
     // Get the global ns
